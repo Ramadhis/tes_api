bf66179a49e26b171fa5d5d09aae160e64261d8c
Ramadhis <M.Ramadhiansyah.I@gmail.com>
2022-06-06 06:23:18 +0700

    tes push after config

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     Route::post('users/signin', 'UserController@signin'); //signin
     
     Route::post('shopping', 'ShoppingController@shopping'); //shopping
-    
+    //tes
     Route::get('users', 'UserController@index'); //all user
 
     Route::post('logout', 'AuthController@logout');
